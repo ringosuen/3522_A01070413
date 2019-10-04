989e3254743c3deb5bfd736212ab479899f22368
Ringo Suen <ringo863@gmail.com>
2019-10-03 23:00:08 -0700

    Add PEP8 styling and comments

--- Labs/Lab 4/dictionary.py
+++ Labs/Lab 4/dictionary.py
@@ -1,20 +1,40 @@
+"""
+This module houses the main method that cues for user input to search for a
+word and gets the definition of the word. Uses the file_handler class and
+loads and writes data externally to a .txt file.
+"""
 import difflib
-
 from file_handler import FileHandler, FileExtensions
 
 
 class Dictionary:
+    """
+    The dictionary class is responsible for loading data into a dictionary
+    and looking up the definition of a word.
+    """
     def __init__(self):
+        """initialize an empty dictionary"""
         self.dictionary = {}
 
     def query_definition(self, word):
+        """
+        Looks up the definition of a word. Also searches for similar words
+        when the particular word is not found.
+        :param word: a string
+        :return: self.dictionary.get(word)
+        """
         word = word.lower()
         if word not in self.dictionary:
+            print("Here are some similar words: ")
             print(difflib.get_close_matches(word, self.dictionary))
         else:
             return self.dictionary.get(word)
 
     def load_dictionary(self, filepath):
+        """
+        Responsible for loading data into a dictionary.
+        :param filepath: a string
+        """
         try:
             data = FileHandler.load_data(filepath, FileExtensions.JSON)
 
@@ -26,6 +46,13 @@ class Dictionary:
 
 
 def main():
+    """
+    Loads the data.json file and asks user for input. Input is a word
+    and the program will output the definition of the word. The user
+    enter exitprogram to exit the program. Search of the word is not case
+    sensitive. If the definition of the word is found, then it create a new
+    existing .txt file and append new words to the file.
+    """
     diction = Dictionary()
     diction.load_dictionary("data.json")
 
--- Labs/Lab 4/file_handler.py
+++ Labs/Lab 4/file_handler.py
@@ -1,22 +1,44 @@
+"""
+This module is responsible for reading a file and handles any error
+with the the file type or file not being found. It also houses a write to file
+method.
+"""
+
 import json
 from enum import Enum
 from pathlib import Path
 
 
 class FileExtensions(Enum):
+    """This is an Enum class for TXT and JSON extensions"""
     TXT = 1
     JSON = 2
 
 
 class InvalidFileTypeError(Exception):
+    """
+    This class handles Invalid File Type Errors.
+    """
     def __init__(self, invalid_file_type_error):
+        """Initiliaze an invalid file type error exception"""
         super.__init__(invalid_file_type_error)
         self.invalidFileType = invalid_file_type_error
 
 
 class FileHandler:
+    """
+    This class is to load data from file as well as writing to an external
+    file.
+    """
     @staticmethod
     def load_data(path, file_type):
+        """
+        Loads data based on the file called and the file type being either TXT
+        or JSON. Also respinsible for raising any exceptions if they occur.
+        :param path: a string
+        :param file_type: an enum extension
+        :return: data
+        """
         my_file = Path(path)
         try:
             if my_file.is_file():
@@ -49,17 +71,11 @@ class FileHandler:
 
     @staticmethod
     def write_lines(path, lines):
-        # print("Writing to saved_words.txt using write mode")
+        """
+        A method to write specified input and output it to an external file as
+        a .txt file.
+        :param path: a string
+        :param lines: a string
+        """
         with open(path, mode='a') as my_text_file:
             my_text_file.write(str(lines))
-
-#
-# def main():
-#     pass
-#     # read_sample_txt_with()
-#     # FileHandler.read_json()
-#     # FileHandler.load_data("data.json", FileExtensions.JSON)
-#
-#
-# if __name__ == '__main__':
-#     main()
