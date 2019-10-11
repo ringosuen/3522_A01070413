2c573e1d6a6963d7f97d049bca729b32610f0fea
Ringo Suen <ringo863@gmail.com>
2019-10-10 23:35:52 -0700

    PEP8 CHECK and COMMENTS

--- Labs/Lab 5/auction_simulator.py
+++ Labs/Lab 5/auction_simulator.py
@@ -6,7 +6,19 @@ import random
 
 
 class Auction:
+    """
+    This class represent an auction object. It is responsible for setting up
+    and starting an auction for an item with a starting price.
+    """
     def __init__(self, bidders, item, starting_price):
+        """
+        Initializes a new Auction with a list of bidders, item and starting
+        price.
+        :param bidders: list
+        :param item: string
+        :param starting_price: int
+        """
+
         self.bidders = bidders
         self.item = item
         self.starting_price = starting_price
@@ -28,6 +40,12 @@ class Auctioneer:
     """
 
     def __init__(self, highest_current_bid):
+        """
+        initializes a new Auctioneer with the current highest current
+        bid state. It also initializes an empty list of bidders.
+        :param highest_current_bid: int
+        """
+
         self.bidder = []
         self.highest_current_bid = highest_current_bid
         self.highest_bidder = "starting price"
@@ -44,10 +62,18 @@ class Auctioneer:
 
 class Bidders:
     """
-    the observer to the auctions being placed.
+    Represents a bidder which acts as an observer to the auctions being placed.
     """
 
     def __init__(self, name, budget, bid_increase_perc):
+        """
+        Initializes a new Bidder with name, budget and bid increase %.
+        Bid probability is using a random value between 0,1.
+        :param name: string
+        :param budget: int
+        :param bid_increase_perc: int
+        """
+
         self.name = name
         self.budget = budget
         self.bid_probability = random.random()
@@ -60,9 +86,8 @@ class Bidders:
     def __call__(self, auctioneer):
         """
         This is the protocol that allows an object to be called and
-        passed around as functions. this method can accept any number
-        of parameters.
-        :param message: an object
+        passed around as functions. This method accepts an auctioneer.
+        :param auctioneer: an object
         """
         if self is auctioneer.highest_bidder:
             print(f"\nThe highest bidder is {self.name}"
@@ -74,14 +99,24 @@ class Bidders:
         # if self.bid_probability > 0.2:
         #     return
         print(
-            f"{self.name} bidded: ${auctioneer.highest_current_bid * self.bid_increase_perc} in response to"
-            f" {auctioneer.highest_bidder}'s ${auctioneer.highest_current_bid}")
-        auctioneer.highest_current_bid = auctioneer.highest_current_bid * self.bid_increase_perc
+            f"{self.name} bidded: "
+            f"${auctioneer.highest_current_bid * self.bid_increase_perc} "
+            f"in response to"
+            f" {auctioneer.highest_bidder}'s "
+            f"${auctioneer.highest_current_bid}")
+        auctioneer.highest_current_bid = \
+            auctioneer.highest_current_bid * self.bid_increase_perc
         auctioneer.highest_bidder = self
         self.highest_bid = auctioneer.highest_current_bid
 
 
 def main():
+    """
+    prompts for user input for an item and item price.
+    prompts the user to enter amount of bidders there are. Then asks user
+    to enter name, their budget and then bidder increase percentage.
+    :return: a bidder list
+    """
     bidders_list = []
     auction_item = input("What item is being auctioned? ")
     auction_price = float(input("What is the price of the item? "))
@@ -109,8 +144,6 @@ def main():
         a1.execute_callbacks()
         temp_max = check_after
         check_after = a1.highest_current_bid
-
-    # print(f"/nHighest Bidder: ")
 
     print("\nHighest Bids Per bidder: ")
     for bidder in a1.bidder:
