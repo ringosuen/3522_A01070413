daf657fbc193127d1e7013340cab79cc9a505e76
Ringo Suen <ringo863@gmail.com>
2019-11-21 09:51:54 -0800

    Initial push

---
+++ Labs/Lab09/producer_consumer.py
@@ -0,0 +1,18 @@
+class CityOverheadTimeQueue:
+    def __init__(self):
+        pass
+
+    def put(self, overhead_time: city_processor.CityOverheadTimes) -> None:
+        pass
+
+    def get(self) -> city_processor.CityOverheadTime:
+        pass
+
+    def __len__(self):
+        pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
