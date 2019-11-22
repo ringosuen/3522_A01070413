73783f1b51bfa51ce71d5243d33c0319e7f881a2
Ringo Suen <ringo863@gmail.com>
2019-11-21 22:03:13 -0800

    Added comments and PEP3 check

--- Labs/Lab09/producer_consumer.py
+++ Labs/Lab09/producer_consumer.py
@@ -5,7 +5,14 @@ import logging
 
 
 class CityOverheadTimeQueue:
+    """
+    This is the buffer that receives a City object and prints out the
+    corresponding info based on the request made.
+    """
     def __init__(self):
+        """
+        Initializes an empty data queue list and a lock.
+        """
         self.data_queue = []
         self.access_queue_lock = threading.Lock()
 
@@ -13,7 +20,7 @@ class CityOverheadTimeQueue:
         """
         Responsible for adding to the queue. Accepts
         a overhead_time parameter and appends it to the data_queue list.
-        :param overhead_time:
+        :param overhead_time: city_processor.CityOverheadTimes
         """
         with self.access_queue_lock:
             self.data_queue.append(overhead_time)
@@ -40,12 +47,27 @@ class CityOverheadTimeQueue:
 
 
 class ProducerThread(threading.Thread):
+    """
+    Send queue to buffer.
+    """
     def __init__(self, cities: list, queue: CityOverheadTimeQueue):
+        """
+        Takes in a list of City objects and queue
+        :param cities: list
+        :param queue: CityOverheadTimeQueue
+        """
         super().__init__()
         self.cities = cities
         self.queue = queue
 
     def run(self) -> None:
+        """
+        Executes when the thread starts. Loops over each city and
+        passes it to the implement_.get_overheadpass() method. It then proceeds
+        to add the city to the queue. After reading in 5 cities, the thread
+        sleeps for 1 secdon.
+        :return: None
+        """
         counter = 0
         for city in self.cities:
             self.queue.put(
@@ -53,25 +75,46 @@ class ProducerThread(threading.Thread):
             counter += 1
             if counter % 5 == 0:
                 time.sleep(1)
-                print("sleeping 1s")
 
 
 class ConsumerThread(threading.Thread):
+    """
+    Responsible for consuming data from the queue and printing it out to
+    the console.
+    """
     def __init__(self, queue: CityOverheadTimeQueue):
+        """
+        Same queue as the one producer has. Implements a data_incoming
+        boolean attribute that is set to True. Changes when a thread has
+        joined.
+        :param queue: CityOverheadTimeQueue
+        """
         super().__init__()
         self.queue = queue
         self.data_incoming = True
 
     def run(self) -> None:
+        """
+        If queue is not empty this method
+        gets an item from the queue and prints it tothe console and
+        then sleep for 0.5s. While processing the queue, if it's empty,
+        puts the trhead to sleep for 0.75s.
+        :return: None
+        """
         if len(self.queue) is 0:
-            print("length is 0")
             time.sleep(0.75)
+            print("sleeping 0.75s")
         while self.data_incoming or len(self.queue) > 0:
             print(self.queue.get())
             time.sleep(0.5)
+            print("sleeping 0.5s")
 
 
 def main():
+    """
+    Uses 3 producer threads and joins together. Reads from an excel file.
+    Excel file is split into 3 parts.
+    """
     excel_file = city_processor.CityDatabase("city_locations.xlsx")
     city_time_queue = CityOverheadTimeQueue()
     city_database = excel_file.city_db
@@ -98,16 +141,6 @@ def main():
     producer3.join()
     consumer.data_incoming = False
     consumer.join()
-
-    # test = CityOverheadTimeQueue()
-    # test_object = city_processor.City("TEST", 10, 20)
-    # test1 = city_processor.ISSDataRequest.get_overhead_pass(test_object)
-    # test.put(test1)
-    # for item in test.data_queue:
-    #     print(item)
-    # print(len(test.data_queue))
-    # test.get()
-    # print(len(test.data_queue))
 
 
 if __name__ == '__main__':
