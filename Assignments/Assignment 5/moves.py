734223d59d3037ad784ca1afddda3d5159033c98
Ringo Suen <ringo863@gmail.com>
2019-11-28 13:14:44 -0800

    Additional Classes made, better structure created.

--- Assignments/Assignment 5/moves.py
+++ Assignments/Assignment 5/moves.py
@@ -5,10 +5,60 @@ import json
 
 
 class Moves:
-    def __init__(self, **kwargs):
-        print(kwargs)
-    @staticmethod
-    async def get_moves_data(id_: str, url: str,
+
+    def __init__(self, name: str, id: int, generation: str, accuracy: int,
+                 pp: int, power: int, type_: str, damage_class: str,
+                 effect: str):
+        self.name = name
+        self.id_ = id
+        self.gen = generation
+        self.accuracy = accuracy
+        self.pp = pp
+        self.power = power
+        self.type_ = type_
+        self.damage_class = damage_class
+        self.effect = effect
+
+    def __str__(self):
+        return f"\nMove Name: {self.name}\n" \
+               f"Move ID: {self.id_}\n" \
+               f"Move Gen: {self.gen}\n" \
+               f"Move Accuracy: {self.accuracy}\n" \
+               f"Move PP: {self.pp}\n" \
+               f"Move Power: {self.power}\n" \
+               f"Move Type: {self.type_}\n" \
+               f"Special or Physical: {self.damage_class}\n" \
+               f"Short Effect: {self.effect}\n"
+
+    @classmethod
+    def create_move_object(cls, move_name_: str):
+        move_object = \
+            asyncio.run(RequestApi.process_single_move_request(move_name_))
+        dump = json.dumps(move_object)
+        move = json.loads(dump)
+
+        move_name = move["name"]
+        move_id = move["id"]
+        move_gen = move["generation"]
+        move_accuracy = move["accuracy"]
+        move_pp = move["pp"]
+        move_power = move["power"]
+        move_type = move["type"]
+        move_damage_class = move["damage_class"]
+        move_short_effect = move["effect_entries"][0]["short_effect"]
+
+        final_object = Moves(move_name, move_id, move_gen, move_accuracy,
+                             move_pp,
+                             move_power, move_type, move_damage_class,
+                             move_short_effect)
+
+        return final_object
+
+
+class RequestApi:
+
+    @classmethod
+    async def get_moves_data(cls, id_: str, url: str,
                              session: aiohttp.ClientSession) -> dict:
         """
         An async coroutine that executes GET http request. The response is
@@ -22,13 +72,13 @@ class Moves:
         target_url = url.format(id_)
         response = await session.request(method="GET", url=target_url,
                                          ssl=ssl.SSLContext())
-        print("Response object from aiohttep:\n", response)
-        print("Response object type:\n", type(response))
+        # print("Response object from aiohttep:\n", response)
+        # print("Response object type:\n", type(response))
         json_dict = await response.json()
         return json_dict
 
-    @staticmethod
-    async def process_single_request(id_) -> dict:
+    @classmethod
+    async def process_single_move_request(cls, id_) -> dict:
         """
         This function depicts the use of await to showcase how one async
         coroutine can await another async coroutine
@@ -39,12 +89,13 @@ class Moves:
         url = "https://pokeapi.co/api/v2/move/{}"
 
         async with aiohttp.ClientSession() as session:
-            response = await Moves.get_moves_data(id_, url, session)
+            response = await RequestApi.get_moves_data(id_, url, session)
 
-            print(response)
+            # print(response)
             return response
-    @staticmethod
-    async def process_requests(requests: list) -> list:
+
+    @classmethod
+    async def process_multiple_move_requests(cls, requests: list) -> list:
         """
         This function depicts the use of asyncio.gather to run multiple
         async coroutines concurrently.
@@ -53,7 +104,7 @@ class Moves:
         """
         url = "https://pokeapi.co/api/v2/move/{}"
         async with aiohttp.ClientSession() as session:
-            async_coroutines = [Moves.get_moves_data(id_, url, session)
+            async_coroutines = [RequestApi.get_moves_data(id_, url, session)
                                 for id_ in requests]
             responses = await asyncio.gather(*async_coroutines)
             for response in responses:
@@ -61,44 +112,82 @@ class Moves:
             return responses
 
 
-def jprint(obj):
-    """
-    Create and print a formatted string of the Python JSON object
-    """
-    #
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
+# def create_move_object(move: str):
+#     move_object = asyncio.run(process_single_request(move))
+#     dump = json.dumps(move_object)
+#     move = json.loads(dump)
+#
+#     move_name = move["name"]
+#     move_id = move["id"]
+#     move_gen = move["generation"]
+#     move_accuracy = move["accuracy"]
+#     move_pp = move["pp"]
+#     move_power = move["power"]
+#     move_type = move["type"]
+#     move_damage_class = move["damage_class"]
+#     move_short_effect = move["effect_entries"][0]["short_effect"]
+#
+#     final_object = Moves(move_name, move_id, move_gen, move_accuracy, move_pp,
+#                          move_power, move_type, move_damage_class,
+#                          move_short_effect)
+#
+#     return final_object
 
 
 def main():
-    move_obj = Moves()
+    # move_obj = Moves()
     move_list = ["pound", "flamethrower", "earthquake"]
-    async_move = asyncio.run(move_obj.process_requests(move_list))
+    async_move = \
+        asyncio.run(RequestApi.process_multiple_move_requests(move_list))
     string_convert = json.dumps(async_move)
-    # print(string_convert)
+    print(string_convert)
     moves_convert_json = json.loads(string_convert)
+    print("\n")
     for move in moves_convert_json:
+        move_name = move["name"]
+        move_id = move["id"]
+        move_gen = move["generation"]
+        move_accuracy = move["accuracy"]
+        move_pp = move["pp"]
+        move_power = move["power"]
         move_type = move["type"]
-        name_type = move["name"]
-        print(name_type, move_type)
-        # return type["type"]
+        move_damage_class = move["damage_class"]
+        move_short_effect = move["effect_entries"][0]["short_effect"]
+        print(move_name, move_id, move_gen, move_accuracy,
+              move_pp, move_power, move_type, move_damage_class,
+              move_short_effect)
 
-    # type = moves_convert_json["name"]
-    # print("asdjfl;kad")
-    # print(type)
-    #
-    #
-    # print(dump)
-
-    # print("-"*900)
-    # pound = asyncio.run(process_single_request("pound"))
+    # pound = asyncio.run(process_single_request("hydro-pump"))
     # dump = json.dumps(pound)
-    # moves_convert_json = json.loads(dump)
-    # type = moves_convert_json["type"]
-    # print(type)
+    # move = json.loads(dump)
+    #
+    # move_name = move["name"]
+    # move_id = move["id"]
+    # move_gen = move["generation"]
+    # move_accuracy = move["accuracy"]
+    # move_pp = move["pp"]
+    # move_power = move["power"]
+    # move_type = move["type"]
+    # move_damage_class = move["damage_class"]
+    # move_short_effect = move["effect_entries"][0]["short_effect"]
+    #
+    # hydro_pump = Moves(move_name, move_id, move_gen, move_accuracy, move_pp,
+    #                    move_power, move_type, move_damage_class,
+    #                    move_short_effect)
+    # print(hydro_pump)
 
-    # d = {'name': 'Oscar', 'lastName': 'Reyes', 'age': 32}
-    # moves = Moves(**d)
+    # print(f"\nMove Name: {move_name}\n"
+    #       f"Move ID: {move_id}\n"
+    #       f"Move Gen: {move_gen}\n"
+    #       f"Move Accuracy: {move_accuracy}\n"
+    #       f"Move PP: {move_pp}\n"
+    #       f"Move Power: {move_power}\n"
+    #       f"Move Type: {move_type}\n"
+    #       f"Special or Physical: {move_damage_class}\n"
+    #       f"Short Effect: {move_short_effect}\n")
+
+    flamethrower = Moves.create_move_object("flamethrower")
+    print(flamethrower)
 
 
 if __name__ == '__main__':
--- Assignments/Assignment 5/pokedex.py
+++ Assignments/Assignment 5/pokedex.py
@@ -2,12 +2,39 @@
 Main driver class to run the program from the terminal.
 """
 import argparse
+import moves
 
-parser = argparse.ArgumentParser()
-# parser.add_argument("pokemon", help="enter the pokemon you want to see")
-parser.add_argument("move", help="enter the move(s) you want to see")
-parser.add_argument("--m", "--move", help="enter the move(s) you want to see")
+def setup_request_commandline():
 
-parser.add_argument("-s", "--string", help="The string of moves")
-args = parser.parse_args()
-print(args.move)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("request", help="enter either pokemon, ability, "
+                                        "or moves, you want to see")
+    parser.add_argument("info", help="enter either the pokemon, ability, "
+                                     "or move")
+    # parser.add_argument("--e", "--move", help="enter the move(s) you want to see")
+    #
+    # parser.add_argument("-s", "--string", help="The string of moves")
+    args = parser.parse_args()
+    type_of_request = args.request
+    second_position = args.info
+    # print(type_of_request)
+    # print(second_position)
+    return type_of_request, second_position
+
+def single_string_info_generator():
+    request = setup_request_commandline()
+    if request[0] == "move":
+        print("USING CREATE MOVE METHOD")
+        test_1 = moves.Moves.create_move_object(request[1])
+        print(test_1)
+
+
+def main():
+    test = single_string_info_generator()
+    # request = setup_request_commandline()
+    #
+    # test_1 = moves.Moves.create_move_object(request[1])
+    # print(test_1)
+
+if __name__ == '__main__':
+    main()
