91734f62b761f10b50543253280fe7f92db00b96
Ringo Suen <ringo863@gmail.com>
2019-12-05 20:22:52 -0800

    Can now grab Abilities one at a time.

--- Assignments/Assignment 5/moves.py
+++ Assignments/Assignment 5/moves.py
@@ -4,6 +4,49 @@ import ssl
 import json
 
 
+class Ability:
+    def __init__(self, name: str, id: int, generation: str, effect: str,
+                 short_effect: str, pokemon: str):
+        self.name = name
+        self.id_ = id
+        self.gen = generation
+        self.effect = effect
+        self.short_effect = short_effect
+        self.pokemon = pokemon
+
+    def __str__(self):
+        return f"\nAbility Name: {self.name.title()}\n" \
+               f"Ability ID: {self.id_}\n" \
+               f"Ability Generation: {self.gen}\n" \
+               f"Long Effect: \n{self.effect}\n\n" \
+               f"Short Effect: {self.short_effect}\n\n" \
+               f"Pokemon that has this ability " \
+               f"listed below:\n-{self.pokemon.title()}\n"
+
+    @classmethod
+    def create_ability_object(cls, ability_name_: str):
+        ability_object = \
+            asyncio.run(
+                RequestApi.process_single_ability_request(ability_name_))
+        dump = json.dumps(ability_object)
+        ability = json.loads(dump)
+
+        ability_name = ability["name"]
+        ability_id = ability["id"]
+        ability_gen = ability["generation"]["name"]
+        ability_long_effect = ability["effect_entries"][0]["effect"]
+        ability_short_effect = ability["effect_entries"][0]["short_effect"]
+        ability_pokemon = "\n-".join([item["pokemon"]["name"] for item in
+                           ability["pokemon"]])
+
+        final_ability_object = Ability(ability_name, ability_id, ability_gen,
+                                       ability_long_effect,
+                                       ability_short_effect,
+                                       ability_pokemon)
+
+        return final_ability_object
+
+
 class Moves:
 
     def __init__(self, name: str, id: int, generation: str, accuracy: int,
@@ -20,9 +63,9 @@ class Moves:
         self.effect = effect
 
     def __str__(self):
-        return f"\nMove Name: {self.name}\n" \
+        return f"\nMove Name: {self.name.title()}\n" \
                f"Move ID: {self.id_}\n" \
-               f"Move Gen: {self.gen}\n" \
+               f"Move Generation: {self.gen}\n" \
                f"Move Accuracy: {self.accuracy}\n" \
                f"Move PP: {self.pp}\n" \
                f"Move Power: {self.power}\n" \
@@ -39,12 +82,12 @@ class Moves:
 
         move_name = move["name"]
         move_id = move["id"]
-        move_gen = move["generation"]
+        move_gen = move["generation"]["name"]
         move_accuracy = move["accuracy"]
         move_pp = move["pp"]
         move_power = move["power"]
-        move_type = move["type"]
-        move_damage_class = move["damage_class"]
+        move_type = move["type"]["name"]
+        move_damage_class = move["damage_class"]["name"]
         move_short_effect = move["effect_entries"][0]["short_effect"]
 
         final_object = Moves(move_name, move_id, move_gen, move_accuracy,
@@ -58,8 +101,8 @@ class Moves:
 class RequestApi:
 
     @classmethod
-    async def get_moves_data(cls, id_: str, url: str,
-                             session: aiohttp.ClientSession) -> dict:
+    async def get_data(cls, id_: str, url: str,
+                       session: aiohttp.ClientSession) -> dict:
         """
         An async coroutine that executes GET http request. The response is
         converted to a json. The HTTP request and the json conversion are
@@ -78,6 +121,22 @@ class RequestApi:
         return json_dict
 
     @classmethod
+    async def process_single_ability_request(cls, id_) -> dict:
+        """
+        This function depicts the use of await to showcase how one async
+        coroutine can await another async coroutine
+        :param id_: an int
+        :return: dict, json response
+        """
+        url = "https://pokeapi.co/api/v2/ability/{}"
+
+        async with aiohttp.ClientSession() as session:
+            response = await RequestApi.get_data(id_, url, session)
+
+            # print(response)
+            return response
+
+    @classmethod
     async def process_single_move_request(cls, id_) -> dict:
         """
         This function depicts the use of await to showcase how one async
@@ -88,7 +147,7 @@ class RequestApi:
         url = "https://pokeapi.co/api/v2/move/{}"
 
         async with aiohttp.ClientSession() as session:
-            response = await RequestApi.get_moves_data(id_, url, session)
+            response = await RequestApi.get_data(id_, url, session)
 
             # print(response)
             return response
@@ -103,11 +162,11 @@ class RequestApi:
         """
         url = "https://pokeapi.co/api/v2/move/{}"
         async with aiohttp.ClientSession() as session:
-            async_coroutines = [RequestApi.get_moves_data(id_, url, session)
+            async_coroutines = [RequestApi.get_data(id_, url, session)
                                 for id_ in requests]
             responses = await asyncio.gather(*async_coroutines)
-            for response in responses:
-                print(response)
+            # for response in responses:
+            #     print(response)
             return responses
 
 
@@ -136,34 +195,8 @@ def main():
     flamethrower = Moves.create_move_object("flamethrower")
     print(flamethrower)
 
-    # pound = asyncio.run(process_single_request("hydro-pump"))
-    # dump = json.dumps(pound)
-    # move = json.loads(dump)
-    #
-    # move_name = move["name"]
-    # move_id = move["id"]
-    # move_gen = move["generation"]
-    # move_accuracy = move["accuracy"]
-    # move_pp = move["pp"]
-    # move_power = move["power"]
-    # move_type = move["type"]
-    # move_damage_class = move["damage_class"]
-    # move_short_effect = move["effect_entries"][0]["short_effect"]
-    #
-    # hydro_pump = Moves(move_name, move_id, move_gen, move_accuracy, move_pp,
-    #                    move_power, move_type, move_damage_class,
-    #                    move_short_effect)
-    # print(hydro_pump)
-
-    # print(f"\nMove Name: {move_name}\n"
-    #       f"Move ID: {move_id}\n"
-    #       f"Move Gen: {move_gen}\n"
-    #       f"Move Accuracy: {move_accuracy}\n"
-    #       f"Move PP: {move_pp}\n"
-    #       f"Move Power: {move_power}\n"
-    #       f"Move Type: {move_type}\n"
-    #       f"Special or Physical: {move_damage_class}\n"
-    #       f"Short Effect: {move_short_effect}\n")
+    intimidate = Ability.create_ability_object("drizzle")
+    print(intimidate)
 
 
 if __name__ == '__main__':
