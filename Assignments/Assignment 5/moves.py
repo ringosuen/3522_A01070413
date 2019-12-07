73d94b9362ab985175e558bdd3595937d2f344e8
Ringo Suen <ringo863@gmail.com>
2019-12-06 23:53:58 -0800

    single string works single string + output works textfile input works textfile to output works

--- Assignments/Assignment 5/moves.py
+++ Assignments/Assignment 5/moves.py
@@ -2,20 +2,7 @@ import aiohttp
 import asyncio
 import ssl
 import json
-
-
-class PokemonMoves:
-    def __init__(self, move_name: list, level_learnt: list):
-        self.move_name = move_name
-        self.level_learnt = level_learnt
-
-
-    def __str__(self):
-
-        dictionary = list(zip(self.move_name, self.level_learnt))
-        for a, b in dictionary:
-            return a
-
+import pokedex
 
 
 class Pokemon:
@@ -24,7 +11,7 @@ class Pokemon:
     """
 
     def __init__(self, name: str, id: int, height: int, weight: int,
-                 stats: dict, types: list, abilities: list, moves: list):
+                 stats: list, types: list, abilities: list, moves: list):
         self.name = name
         self.id_ = id
         self.height = height
@@ -34,28 +21,28 @@ class Pokemon:
         self.abilities = abilities
         self.moves = moves
 
+    """modified one"""
+
     def __str__(self):
         return f"\nPokemon Name: {self.name.title()}\n" \
                f"Pokemon ID: {self.id_}\n" \
                f"Pokemon Height: {self.height} decimeters\n" \
                f"Pokemon Weight: {self.weight} hectograms\n" \
-               f"Pokemon Stat: {self.stats}\n" \
                f"Pokemon Type: {self.types}\n" \
-               f"Pokemon Abilities: {self.abilities}\n" \
-               f"Pokemon Move & Level Learnt: {self.moves}\n"
+               f"Pokemon Abilities: {self.abilities}\n"
+
     @classmethod
-    def get_move_url(cls, pokemon_name : str):
+    def get_move_url(cls, pokemon_name: str):
         pokemon_object = \
             asyncio.run(
                 RequestApi.process_single_pokemon_request(pokemon_name))
         pokemon_dump = json.dumps(pokemon_object)
         pokemon = json.loads(pokemon_dump)
 
-        ability_url = [item["ability"]["url"] for item in pokemon ["abilities"]]
+        ability_url = [item["ability"]["url"] for item in pokemon["abilities"]]
         print(ability_url)
 
         return ability_url
-
 
     @classmethod
     def create_pokemon_object(cls, pokemon_name_: str):
@@ -71,7 +58,9 @@ class Pokemon:
         pokemon_weight = pokemon["weight"]
         stat_name = [item["stat"]["name"] for item in pokemon["stats"]]
         base_stat = [item["base_stat"] for item in pokemon["stats"]]
-        pokemon_stats = dict(zip(stat_name, base_stat))
+        # stat_url = [item["stat"]["url"] for item in pokemon["stats"]]
+
+        pokemon_stats = list(zip(stat_name, base_stat))
         pokemon_type = [item["type"]["name"] for item in pokemon["types"]]
         pokemon_abilities = [item["ability"]["name"] for item in
                              pokemon["abilities"]]
@@ -85,13 +74,14 @@ class Pokemon:
         move = [item1["move"]["name"] for item1 in pokemon["moves"]]
         level = [item1["version_group_details"][0]["level_learned_at"] for
                  item1 in pokemon["moves"]]
+        move_url = [item1["move"]["url"] for item1 in pokemon["moves"]]
         # print(move, level)
         moves = list(zip(move, level))
         # for a,b in moves:
         #     print(a,b)
-        pokemon_moves = PokemonMoves(move, level)
+        # pokemon_moves = PokemonMoves(move, level)
 
-        ability_url = [item["ability"]["url"] for item in pokemon ["abilities"]]
+        ability_url = [item["ability"]["url"] for item in pokemon["abilities"]]
         print(ability_url)
 
         final_ability_object = Pokemon(pokemon_name,
@@ -198,6 +188,67 @@ class Moves:
                              move_short_effect)
 
         return final_object
+
+    @classmethod
+    def create_multiple_ability_objects(cls, ability_name_: list):
+        request = pokedex.setup_request_commandline()
+        async_move = \
+            asyncio.run(RequestApi.process_multiple_ability_requests(ability_name_))
+        string_convert = json.dumps(async_move)
+        ability_convert_json = json.loads(string_convert)
+
+        print("\n")
+        for ability in ability_convert_json:
+            ability_name = ability["name"]
+            ability_id = ability["id"]
+            ability_gen = ability["generation"]["name"]
+            ability_long_effect = ability["effect_entries"][0]["effect"]
+            ability_short_effect = ability["effect_entries"][0]["short_effect"]
+            ability_pokemon = "\n-".join([item["pokemon"]["name"] for item in
+                                          ability["pokemon"]])
+
+            final_ability_object = Ability(ability_name, ability_id,
+                                           ability_gen,
+                                           ability_long_effect,
+                                           ability_short_effect,
+                                           ability_pokemon)
+            if request[0].lower() == "ability" and request[3] is None:
+                print(final_ability_object)
+            if request[0].lower() == "ability" and request[3] is not None:
+                with open(request[3], mode="a") \
+                        as output_file:
+                    output_file.write(str(final_ability_object))
+
+    @classmethod
+    def create_multiple_move_objects(cls, move_name_: list):
+        request = pokedex.setup_request_commandline()
+        async_move = \
+            asyncio.run(RequestApi.process_multiple_move_requests(move_name_))
+        string_convert = json.dumps(async_move)
+        moves_convert_json = json.loads(string_convert)
+
+        print("\n")
+        for move in moves_convert_json:
+            move_name = move["name"]
+            move_id = move["id"]
+            move_gen = move["generation"]["name"]
+            move_accuracy = move["accuracy"]
+            move_pp = move["pp"]
+            move_power = move["power"]
+            move_type = move["type"]["name"]
+            move_damage_class = move["damage_class"]["name"]
+            move_short_effect = move["effect_entries"][0]["short_effect"]
+            final_move_object = Moves(move_name, move_id, move_gen,
+                                      move_accuracy,
+                                      move_pp,
+                                      move_power, move_type, move_damage_class,
+                                      move_short_effect)
+            if request[0].lower() == "move" and request[3] is None:
+                print(final_move_object)
+            if request[0].lower() == "move" and request[3] is not None:
+                with open(request[3], mode="a") \
+                        as output_file:
+                    output_file.write(str(final_move_object))
 
 
 class RequestApi:
@@ -321,9 +372,9 @@ class RequestApi:
             #     print(response)
             return responses
 
-
     @classmethod
-    async def expanded_process_multiple_pokemon_requests(cls, requests: list) -> list:
+    async def expanded_process_multiple_pokemon_requests(cls,
+                                                         requests: list) -> list:
         """
         This function depicts the use of asyncio.gather to run multiple
         async coroutines concurrently.
@@ -335,64 +386,38 @@ class RequestApi:
             async_coroutines = [RequestApi.get_data(id_, url, session)
                                 for id_ in requests]
             responses = await asyncio.gather(*async_coroutines)
-            for response in responses:
-                print(response)
+            # for response in responses:
+            #     print(response)
             return responses
 
 
-
 def main():
-    # ditto = Pokemon.get_move_url("vileplume")
-    # print(ditto)
-    move_list = ["pound", "flamethrower", "earthquake"]
-    # url_list = ['https://pokeapi.co/api/v2/ability/27/', 'https://pokeapi.co/api/v2/ability/34/']
-    ability_Vile = Pokemon.create_pokemon_object("vileplume")
-    url_list = ability_Vile[1]
-    async_move = \
-        asyncio.run(RequestApi.process_multiple_move_requests(move_list))
-    async_ability_expanded = \
-        asyncio.run(RequestApi.expanded_process_multiple_pokemon_requests(url_list))
-    string_convert = json.dumps(async_move)
-    ability_expanded_dump = json.dumps(async_ability_expanded)
-    # print(string_convert)
-    moves_convert_json = json.loads(string_convert)
-    ability_expanded_query = json.loads(ability_expanded_dump)
-    print("\nHERE'S THE EXPANDED ABILITY FROM ABOVE, SEE BELOW:")
-    for ability in ability_expanded_query:
-        ability_name = ability["name"]
-        ability_id = ability["id"]
-        ability_gen = ability["generation"]["name"]
-        ability_long_effect = ability["effect_entries"][0]["effect"]
-        ability_short_effect = ability["effect_entries"][0]["short_effect"]
-        ability_pokemon = "\n-".join([item["pokemon"]["name"] for item in
-                                      ability["pokemon"]])
-        # print(ability_name, ability_id, ability_gen,ability_long_effect, ability_short_effect, ability_pokemon)
-        final_ability_object = Ability(ability_name, ability_id, ability_gen,
-                                       ability_long_effect,
-                                       ability_short_effect,
-                                       ability_pokemon)
-        print(final_ability_object)
-
-    print("\n")
-    # for move in moves_convert_json:
-    #     move_name = move["name"]
-    #     move_id = move["id"]
-    #     move_gen = move["generation"]["name"]
-    #     move_accuracy = move["accuracy"]
-    #     move_pp = move["pp"]
-    #     move_power = move["power"]
-    #     move_type = move["type"]["name"]
-    #     move_damage_class = move["damage_class"]["name"]
-    #     move_short_effect = move["effect_entries"][0]["short_effect"]
-    #     # print(move_name, move_id, move_gen, move_accuracy,
-    #     #       move_pp, move_power, move_type, move_damage_class,
-    #     #       move_short_effect)
-    #     final_object = Moves(move_name, move_id, move_gen, move_accuracy,
-    #                          move_pp,
-    #                          move_power, move_type, move_damage_class,
-    #                          move_short_effect)
-    #     print(final_object)
-
+    move_list = ["tackle", "flamethrower", "earthquake"]
+    Moves.create_multiple_move_objects(move_list)
+    # # url_list = ['https://pokeapi.co/api/v2/ability/27/', 'https://pokeapi.co/api/v2/ability/34/']
+    # ability_Vile = Pokemon.create_pokemon_object("vileplume")
+    # url_list = ability_Vile[1]
+    # async_ability_expanded = \
+    #     asyncio.run(
+    #         RequestApi.expanded_process_multiple_pokemon_requests(url_list))
+    # ability_expanded_dump = json.dumps(async_ability_expanded)
+    # # print(string_convert)
+    # ability_expanded_query = json.loads(ability_expanded_dump)
+    # print("\nHERE'S THE EXPANDED ABILITY FROM ABOVE, SEE BELOW:")
+    # for ability in ability_expanded_query:
+    #     ability_name = ability["name"]
+    #     ability_id = ability["id"]
+    #     ability_gen = ability["generation"]["name"]
+    #     ability_long_effect = ability["effect_entries"][0]["effect"]
+    #     ability_short_effect = ability["effect_entries"][0]["short_effect"]
+    #     ability_pokemon = "\n-".join([item["pokemon"]["name"] for item in
+    #                                   ability["pokemon"]])
+    #     # print(ability_name, ability_id, ability_gen,ability_long_effect, ability_short_effect, ability_pokemon)
+    #     final_ability_object = Ability(ability_name, ability_id, ability_gen,
+    #                                    ability_long_effect,
+    #                                    ability_short_effect,
+    #                                    ability_pokemon)
+    #     print(final_ability_object)
 
     # flamethrower = Moves.create_move_object("flamethrower")
     # print(flamethrower)
--- Assignments/Assignment 5/pokedex.py
+++ Assignments/Assignment 5/pokedex.py
@@ -3,6 +3,9 @@ Main driver class to run the program from the terminal.
 """
 import argparse
 import moves
+import enum
+
+
 
 
 def setup_request_commandline():
@@ -20,12 +23,14 @@ def setup_request_commandline():
     parser.add_argument("-o", "--output", help="Default out put is console.")
     try:
         args = parser.parse_args()
-        print(args.__dict__)
+        # print(args.__dict__)
         type_of_request = args.request
         second_position = args.info
+        pokedex_state = args.expanded
+        pokedex_output = args.output
         # print(type_of_request)
         # print(second_position)
-        return type_of_request, second_position
+        return type_of_request, second_position, pokedex_state, pokedex_output
     except Exception as e:
         print(f"Error! Could not read arguments.\n{e}")
         quit()
@@ -36,27 +41,127 @@ class HandleRequest:
     @classmethod
     def process_single_string(cls):
         request = setup_request_commandline()
-        if request[0] == "move":
+
+        if request[0].lower() == "move" and request[3] is None:
             print("USING CREATE MOVE METHOD")
             generate_move = moves.Moves.create_move_object(request[1])
             print(generate_move)
+
+        elif request[0].lower() == "move" and request[3] is not None:
+            generate_move = moves.Moves.create_move_object(request[1])
+            print("Writing Output to file")
+            with open(request[3], mode="a") \
+                    as output_file:
+                output_file.write(str(generate_move))
             return generate_move
 
-        elif request[0] == "pokemon":
+        elif request[0] == "pokemon" and request[3] is None:
             print("USING CREATE POKEMON METHOD")
+            generate_pokemon = moves.Pokemon.create_pokemon_object(request[1])
+            print(generate_pokemon[0])
+            print(f"{generate_pokemon[0].name.title()} Stats:")
+            for stats in generate_pokemon[0].stats:
+                print(stats)
+            print(f"\n{generate_pokemon[0].name.title()} "
+                  f"Moves and Level Learnt:")
+            for move_, level in generate_pokemon[0].moves:
+                print(f"{move_} learnt at {level}")
+            # print(generate_pokemon[0].stats)
+            return generate_pokemon
 
-        elif request[0] == "ability":
+        elif request[0].lower() == "pokemon" and request[3] is not None:
+            generate_pokemon = moves.Pokemon.create_pokemon_object(request[1])
+            print("Writing Output to file")
+            with open(request[3], mode="a") \
+                    as output_file:
+                output_file.write("\n" + str(generate_pokemon[0]))
+                output_file.write("Stats:")
+                for stats in generate_pokemon[0].stats:
+                    output_file.write(str(stats))
+                output_file.write("\nMoves and Level Learnt:")
+                for move_, level, url in generate_pokemon[0].moves:
+                    output_file.write("\n-" + str(move_) + " learnt at "
+                                      + str(level) + ", url " + str(url))
+
+        elif request[0].lower() == "ability" and request[3] is None:
             print("USING ABILITY METHOD")
+            generate_ability = moves.Ability.create_ability_object(request[1])
+            print(generate_ability)
+            return generate_ability
+        elif request[0].lower() == "ability" and request[3] is not None:
+            generate_ability = moves.Ability.create_ability_object(request[1])
+            with open(request[3], mode="a") \
+                    as output_file:
+                output_file.write("\n" + str(generate_ability))
+
         else:
             print("Not a valid mode")
 
-    def process_text_file(self):
-        pass
+    @classmethod
+    def process_text_file(cls, text_file):
+        request = setup_request_commandline()
+        print("Checking mode type")
+        if request[0].lower() == "move" and request[3] is None:
+            with open(text_file, mode="r") as move_file:
+                file_text = move_file.readlines()
+                # print(file_text)
+                move_list = [move.strip() for move in file_text]
+            generate_moves = \
+                moves.Moves.create_multiple_move_objects(move_list)
+        elif request[0].lower() == "move" and request[3] is not None:
+            print("Writing input_file to output file data")
+            with open(text_file, mode="r") as move_file:
+                file_text = move_file.readlines()
+                move_list = [move.strip() for move in file_text]
+            generate_moves = \
+                moves.Moves.create_multiple_move_objects(move_list)
+
+        elif request[0].lower() == "ability" and request[3] is None:
+            with open(text_file, mode="r") as move_file:
+                file_text = move_file.readlines()
+                # print(file_text)
+                ability_list = [move.strip() for move in file_text]
+            generate_ability = \
+                moves.Moves.create_multiple_ability_objects(ability_list)
+        elif request[0].lower() == "ability" and request[3] is not None:
+            print("Writing input_file to output file data")
+            with open(text_file, mode="r") as move_file:
+                file_text = move_file.readlines()
+                ability_list = [move.strip() for move in file_text]
+            generate_ability = \
+                moves.Moves.create_multiple_ability_objects(ability_list)
+
+
+                # for item in generate_moves:
+                #     output_file.write("\n" + str(item))
+
+
 
 
 def main():
-    test = HandleRequest()
-    test.process_single_string()
+    pokedex = HandleRequest()
+    # pokedex.process_single_string()
+
+    request = setup_request_commandline()
+    # print(request[0])
+    # print(request[1])
+    # print(request[2])
+    print(request[3])
+    # if request[3] is None and request[2] is True:
+    #     print("Single String + Expansion")
+    # elif request[3] is None:
+    #     print("\nMode Detected: Single String + No Expansion")
+    #     pokedex.process_single_string()
+    # elif request[3] is not None and request[2] is True:
+    #     print("Text File + expansion")
+    # else:
+    #     pokedex.process_text_file()
+    if request[1].endswith(".txt"):
+        print("Using text file method")
+        pokedex.process_text_file(request[1])
+    elif request[1].endswith(""):
+        print("single string, no expansion, not text file")
+        pokedex.process_single_string()
 
 
 if __name__ == '__main__':
